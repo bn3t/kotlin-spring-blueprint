c6ba898bb51fb4eb526c84a142e68e554fe7e564
Bernard Niset <bernard.niset@gmail.com>
2024-09-23 07:40:15 +0200

    Add integration tests with WebTestClient

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-insert into book (id, title,  isbn) values (1, 'Java Programming', '1234567890');
+insert into book (id, title, isbn)
+values (1, 'Java Programming', '1234567890');
+
+insert into book (id, title, isbn)
+values (2, 'Kotlin Programming', '1234567891');
